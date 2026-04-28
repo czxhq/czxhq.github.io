f7e79f4cbcc2ec58e8c7785350d035d396560040
czxhq <22373385@buaa.edu.cn>
2026-04-29 00:41:51 +0800

    Fix KaTeX accent rendering

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import './assets/markdown-style.css'
+import 'katex/dist/katex.min.css'
 import App from './App.vue'
 import router from './router'
 import { MotionPlugin } from '@vueuse/motion'
